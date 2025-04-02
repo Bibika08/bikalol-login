cc99af655969e03afc61e208125c3d5f89db843d
Student <student@compass.local>
2025-04-02 11:16:29 +0600

    classwork bibika

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyB0JT1YaC6vxMWu2-EDqIUR7V7Td2R6bZU",
-    authDomain: "bikalol-73db8.firebaseapp.com",
-    projectId: "bikalol-73db8",
-    storageBucket: "bikalol-73db8.firebasestorage.app",
-    messagingSenderId: "496960355401",
-    appId: "1:496960355401:web:31644561a66c8832205398"
-  };
+  apiKey: "AIzaSyB0JT1YaC6vxMWu2-EDqIUR7V7Td2R6bZU",
+  authDomain: "bikalol-73db8.firebaseapp.com",
+  databaseURL: "https://bikalol-73db8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bikalol-73db8",
+  storageBucket: "bikalol-73db8.firebasestorage.app",
+  messagingSenderId: "496960355401",
+  appId: "1:496960355401:web:31644561a66c8832205398"
+};
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase
 export const auth = getAuth(app);
